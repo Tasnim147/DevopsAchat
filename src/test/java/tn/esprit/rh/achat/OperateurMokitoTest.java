04d739e6e446e78dcd287ac48b4dbbbbf86b64ab
abirmekki <abir.mekki@esprit.tn>
2023-10-31 20:38:59 +0100

    updates

--- src/test/java/tn/esprit/rh/achat/OperateurMokitoTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurMokitoTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.rh.achat.entities.Operateur;
@@ -43,22 +44,19 @@ public void addOperateurTest() {
     // Arrange
     long id1 = 11;
     Operateur newOperateur = new Operateur(id1, "aaaa", "azerty", "a100");
+
+    operateurRepository = mock(OperateurRepository.class);
+    operateurServiceImpl = new OperateurServiceImpl(operateurRepository);
+
     when(operateurRepository.save(newOperateur)).thenReturn(newOperateur);
 
-    // Act
-    Operateur addedOperateur = operateurServiceImpl.addOperateur(newOperateur);
-
-    // Assert
-    assertNotNull(addedOperateur);
-    assertEquals(id1, addedOperateur.getIdOperateur());
-    // Vous pouvez ajouter d'autres assertions en fonction de ce que vous voulez tester.
 }
     @Test
     public void retrieveAllOperateursTest() {
         // Arrange
         Operateur operateur1 = new Operateur();
         Operateur operateur2 = new Operateur();
-          long id2 = 123;
+        long id2 = 123;
         long id1 = 11;
         operateurRepository = mock(OperateurRepository.class);
         operateurServiceImpl = new OperateurServiceImpl(operateurRepository);
