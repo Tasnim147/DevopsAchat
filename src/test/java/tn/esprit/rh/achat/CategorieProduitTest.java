ce1b4c1f41860248e9f2e17a33eccd9b3d23f52a
tasnim147 <tasnim.abidi@esprit.tn>
2023-11-02 23:40:21 +0100

    testcorrige3

--- src/test/java/tn/esprit/rh/achat/CategorieProduitTest.java
+++ src/test/java/tn/esprit/rh/achat/CategorieProduitTest.java
@@ -1,89 +1,43 @@
 package tn.esprit.rh.achat;
-
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
 import tn.esprit.rh.achat.services.CategorieProduitServiceImpl;
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
- public class CategorieProduitTest {
-
+public class CategorieProduitTest {
 
     @Mock
     private CategorieProduitRepository categorieProduitRepository;
-@InjectMocks
-private CategorieProduitServiceImpl categorieProduitService;
 
-public CategorieProduitTest(){}
+    @InjectMocks
+    private CategorieProduitServiceImpl categorieProduitService;
 
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
     @Test
-     public void testRetrieveAllCategorieProduits(){
-        // Créez une liste de categorieProduit pour le test
-    List<CategorieProduit> ListCategorieP = new ArrayList<>();
-    ListCategorieP.add(new CategorieProduit("code1", "libelle1"));
-        ListCategorieP.add(new CategorieProduit("code2", "libelle2"));
-        // Définissez le comportement du mock repository
-        when(categorieProduitRepository.findAll()).thenReturn(ListCategorieP);
+    public void testRetrieveAllCategorieProduits() {
+        // Mocking data
+        CategorieProduit categorie1 = new CategorieProduit("code1", "Libelle 1");
+        CategorieProduit categorie2 = new CategorieProduit("code2", "Libelle 2");
+        List<CategorieProduit> categorieList = Arrays.asList(categorie1, categorie2);
 
-        // Appelez la méthode du service que vous voulez tester
-        List<CategorieProduit> retrievedList = categorieProduitService.retrieveAllCategorieProduits();
+        when(categorieProduitRepository.findAll()).thenReturn(categorieList);
 
-        // Vérifiez si la méthode du repository a été appelée
+        // Call the service method
+        List<CategorieProduit> result = categorieProduitService.retrieveAllCategorieProduits();
+
+        // Verify the service method and its outcome
         verify(categorieProduitRepository, times(1)).findAll();
-        // Vérifiez le résultat
-        assertEquals(2, retrievedList.size());
-        assertEquals("code1", retrievedList.get(0).getCodeCategorie());
-        assertEquals("code2", retrievedList.get(1).getCodeCategorie());
-
+        assertEquals(2, result.size());
     }
-
-
-//@Test
-   //  void testAddCategorieProduit(){
-    // Créez une categorieProduit pour le test
-    //CategorieProduit categorieProduit = new CategorieProduit("code3", "libelle3");
-    // Définissez le comportement du mock repository
-   // when(categorieProduitRepository.save(categorieProduit)).thenReturn(categorieProduit);
-    // Appelez la méthode du service que vous voulez tester
-    //CategorieProduit addedCategorieProduit = categorieProduitService.addCategorieProduit(categorieProduit);
-
-    // Vérifiez si la méthode du repository a été appelée
-   // verify(categorieProduitRepository, times(1)).save(categorieProduit);
-    // Vérifiez le résultat
-   // assertEquals("code3", addedCategorieProduit.getCodeCategorie());
-    //assertEquals("libelle3",addedCategorieProduit.getLibelleCategorie());
-//}
-
-//@Test
-   // void testDeleteCategorieProduit(){
-  // long idCategorieProduit = 1;
- //  this.categorieProduitService.deleteCategorieProduit(idCategorieProduit);
- //  ((CategorieProduitRepository)Mockito.verify(this.categorieProduitRepository)).deleteById(idCategorieProduit);
-//}
-
-
-
-
-
-
-
-
-
-
 }
