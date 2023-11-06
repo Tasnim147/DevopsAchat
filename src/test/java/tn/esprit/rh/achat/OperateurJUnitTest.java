c13ed398fb84e6453971fc28606037bb9d246574
abirmekki <abir.mekki@esprit.tn>
2023-11-06 17:21:19 +0100

    JUnit updates

--- src/test/java/tn/esprit/rh/achat/OperateurJUnitTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurJUnitTest.java
@@ -1,6 +1,9 @@
 package tn.esprit.rh.achat;
 
 
+import nonapi.io.github.classgraph.concurrency.AutoCloseableExecutorService;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,58 +15,116 @@ import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
 
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 
 public class OperateurJUnitTest {
 
-//   @Autowired
-//     OperateurRepository operateurRepository;
-//    @Autowired
-//     OperateurServiceImpl operateurServiceImpl;
-//
-//
-//  @Test
-//    public void addOperateurTest() {
-//        // Arrange
-//        Operateur operateur = new Operateur();
-//      operateur.setNom("abir");
-////        // Act
-//       Operateur result = operateurServiceImpl.addOperateur(operateur);
-////
-////        // Assert
-//       assertNotNull(result); // Vérifie que l'opérateur retourné n'est pas nul
-//       assertEquals(operateur, result); // Vérifie que l'opérateur retourné est le même que celui que vous avez passé
-//       }
-//
-//   @Test
-// public void retrieveOperateurTest() {
-////        // Arrange
-//    Long id = 1L; // L'ID que vous souhaitez utiliser
-//     Operateur operateur = new Operateur();
-////        /////////  operateur.setNom("NomOpérateur");
-//        operateurRepository.save(operateur);
-////
-////        // Act
-//        Operateur result = operateurServiceImpl.retrieveOperateur(operateur.getIdOperateur());
-////
-////        // Assert
-//      assertNotNull(result); // Vérifie que l'objet renvoyé n'est pas null
-//       assertEquals(operateur.getIdOperateur(), result.getIdOperateur()); // Vérifie que les ID correspondent
-//       assertEquals(operateur.getNom(), result.getNom());// Vérifie le Nom
-////
-////        ///////// assertEquals("NomOpérateur", result.getNom()); // Vérifie que le Nom correspond
-////        // Ajoutez d'autres assertions pour vérifier d'autres propriétés si nécessaire
-//
-//
-//  }
-//
-//
-//
-//
+   @Autowired
+     OperateurRepository operateurRepository;
+    @Autowired
+    OperateurServiceImpl operateurServiceImpl;
+
+    @Test
+    void testRetrieveAllOperateurs() {
+        ArrayList<Operateur> operateurList = new ArrayList<>();
+        operateurRepository.saveAll(operateurList);
+
+        List<Operateur> actualRetrieveAllOperateursResult = operateurServiceImpl.retrieveAllOperateurs();
+
+        assertNotSame(operateurList, actualRetrieveAllOperateursResult);
+        assertFalse(actualRetrieveAllOperateursResult.isEmpty());
+    }
+    @Test
+    void testAddOperateur() {
+
+        Operateur newOperateur = new Operateur();
+        newOperateur.setNom("karim");
+        newOperateur.setPrenom("mejbri");
+        newOperateur.setPassword("0");
+
+        Operateur addedOperateur = operateurServiceImpl.addOperateur(newOperateur);
+
+        assertNotNull(addedOperateur);
+
+        Operateur retrievedOperateur = operateurRepository.findById(addedOperateur.getIdOperateur()).orElse(null);
+
+        assertNotNull(retrievedOperateur);
+        assertEquals("karim", retrievedOperateur.getNom());
+        assertEquals("mejbri", retrievedOperateur.getPrenom());
+        assertEquals("0", retrievedOperateur.getPassword());
+    }
+
+    @Test
+    void testDeleteOperateur() {
+        // Create a new Operateur object
+        Operateur newOperateur = new Operateur();
+        newOperateur.setNom("testoperature");
+
+        // Save the Operateur using the repository
+        Operateur addedOperateur = operateurRepository.save(newOperateur);
+
+        // Call the method to delete the Operateur by its ID
+        operateurServiceImpl.deleteOperateur(addedOperateur.getIdOperateur());
+
+        // Try to retrieve the deleted Operateur from the repository
+        Optional<Operateur> deletedOperateur = operateurRepository.findById(addedOperateur.getIdOperateur());
+
+        // Assert that the deleted Operateur is not found
+        assertFalse(deletedOperateur.isPresent());
+    }
+
+
+    @Test
+    void testUpdateOperateur() {
+
+        Operateur newOperateur = new Operateur();
+        newOperateur.setNom("testoperature2");
+
+
+        Operateur addedOperateur = operateurRepository.save(newOperateur);
+
+
+        addedOperateur.setNom("updatedtestoperature2");
+
+
+        Operateur updatedOperateur = operateurServiceImpl.updateOperateur(addedOperateur);
+
+
+        Operateur retrievedOperateur = operateurRepository.findById(addedOperateur.getIdOperateur()).orElse(null);
+
+
+        assertEquals("updatedtestoperature2", retrievedOperateur.getNom());
+    }
+    @Test
+    void testRetrieveOperateur() {
+
+        Operateur newOperateur = new Operateur();
+        newOperateur.setNom("test3");
+        newOperateur.setPrenom("test3p");
+        newOperateur.setPassword("12345");
+
+
+        Operateur addedOperateur = operateurServiceImpl.addOperateur(newOperateur);
+
+
+        Operateur retrievedOperateur = operateurServiceImpl.retrieveOperateur(addedOperateur.getIdOperateur());
+
+
+        assertNotNull(retrievedOperateur);
+
+
+        assertEquals("test3", retrievedOperateur.getNom());
+        assertEquals("test3p", retrievedOperateur.getPrenom());
+        assertEquals("12345", retrievedOperateur.getPassword());
+    }
 
 
 }
