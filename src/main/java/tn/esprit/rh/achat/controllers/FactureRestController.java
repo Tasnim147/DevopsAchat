704aadc545c3b4936b0b3b1c6a9f3a5cf4b43bb8
dhaouadinourr <98426941+dhaouadinourr@users.noreply.github.com>
2023-11-09 01:25:35 +0100

    Update FactureRestController.java

--- src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @Api(tags = "Gestion des factures")
-@RequestMapping("/facture")
+@RequestMapping("/SpringMVC/facture")
 @CrossOrigin("*")
 public class FactureRestController {
 
